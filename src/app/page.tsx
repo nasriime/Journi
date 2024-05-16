5e8bc38614e559ac79bd65f67588d75be5f97d1c
nasriime <mnasr4040@gmail.com>
2024-05-16 04:06:05 +0200

    styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,23 +25,23 @@ export default function HomePage() {
 
   return (
     <main>
-      <section className='mx-auto max-w-2xl'>
-        <div className='layout relative flex min-h-screen flex-col items-center justify-center py-12 text-center'>
+      <section className="mx-auto max-w-2xl">
+        <div className="layout relative flex min-h-screen flex-col justify-center py-12">
           <h2 className="mb-8">Find your closest countries</h2>
-        <AsyncSelect
-          instanceId={useId()}
-          className="w-full"
-          classNamePrefix="select"
-          isClearable={true}
-          isSearchable={true}
-          name="country"
-          placeholder="Select a country"
-          loadOptions={promiseOptions}
-          onChange={(option) => onChange((option as OptionType))}
-          />
+          <AsyncSelect
+            instanceId={useId()}
+            className="w-full"
+            classNamePrefix="select"
+            isClearable={true}
+            isSearchable={true}
+            name="country"
+            placeholder="Select a country"
+            loadOptions={promiseOptions}
+            onChange={(option) => onChange((option as OptionType))}
+          />  
           {selectedCountry && <div className="mt-8">
-            <h4>{selectedCountry.name}</h4>
-            <h4>{selectedCountry.capital}</h4>
+            <h4>Country: {selectedCountry.name}</h4>
+            <h4>Capital: {selectedCountry.capital}</h4>
           </div>}
         </div>
       </section>
